92e90b167c8aeeb5b88a0150a21af37507220eb9
Abhay Yadav <yadavabhay966@gmail.com>
2024-03-12 15:29:18 +0530

    API updated

--- src/hooks/useGif.js
+++ src/hooks/useGif.js
@@ -15,7 +15,8 @@ function useGif(tag) {
   const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
   // api for random
-  const randomMemeUrl = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`
+  const randomMemeUrl = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=&rating=g`
+  
 
   // api url for tag means in which we are passing input
   const tagMemeUrl = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`;
